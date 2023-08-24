9e0281c192198c4a5917b799b605e1a47d94fa55
ibnufathoni <133759969+ibnufathoni@users.noreply.github.com>
2023-08-24 21:31:26 +0700

    feat: add payment feature (#16)
    * feat: add payment feature
    
    * fix: update payment
    
    * fix: update to  absolute path

--- src/common/router/router-list.js
+++ src/common/router/router-list.js
@@ -1,9 +1,11 @@
 import { createBrowserRouter } from 'react-router-dom';
-import LandingPage from '../../module/landingPage';
-import CartPage from '../../module/cart';
-import Login from '../../module/login';
-import Registration from '../../module/registration';
-import LoginAdmin from '../../module/admin/login';
+import LandingPage from 'src/module/landingPage';
+import CartPage from 'src/module/cart';
+import Login from 'src/module/login';
+import Registration from 'src/module/registration';
+import LoginAdmin from 'src/module/admin/login';
+import Payment from 'src/module/payment';
+import PaymentDetail from 'src/module/payment/pages/PaymentDetail';
 
 const routerList = createBrowserRouter([
   {
@@ -25,6 +27,14 @@ const routerList = createBrowserRouter([
   {
     path: 'admin/login',
     element: <LoginAdmin />,
+  },
+  {
+    path: 'cart/payment/:id',
+    element: <Payment />,
+  },
+  {
+    path: 'cart/payment-detail/:id',
+    element: <PaymentDetail />,
   },
 ]);
 
